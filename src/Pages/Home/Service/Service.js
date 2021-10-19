817d0c0331bbf516bcce32acbaa2480e36627553
bakhtiarabid <abid.bu16@gmail.com>
2021-10-19 09:59:25 +0600

    service section updated

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,13 +1,18 @@
 import React from "react";
+import "./Service.css";
 
 const Service = ({ service }) => {
    const { img, name, desc } = service;
    return (
       <div className="service pb-3">
          <img className="img-fluid" src={img} alt="" />
-         <h3>{name}</h3>
-         <button className="btn btn-warning">Book {name.toLowerCase()}</button>
-         <p className="px-3">{desc}</p>
+         <h4 className="ms-2">{name}</h4>
+         <p className="px-3">{desc.slice(0, 175)}...</p>
+         <div className="text-center ">
+            <button className="btn btn-primary btn-style pe-5 ps-5">
+               Details
+            </button>
+         </div>
       </div>
    );
 };
