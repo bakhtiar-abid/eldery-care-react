ba16d0fd6df2fdf87eda250a49bfa79c57c9a093
bakhtiarabid <abid.bu16@gmail.com>
2021-10-19 07:35:40 +0600

    Header Section Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import AuthProvider from "./contexts/AuthProvider";
+
+import Header from "./Shared/Header/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login/Login";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <AuthProvider>
+            <Router>
+               <Header></Header>
+               <Switch>
+                  <Route exact path="/">
+                     <Home></Home>
+                  </Route>
+                  <Route path="/home">
+                     <Home></Home>
+                  </Route>
+                  <Route path="/login">
+                     <Login></Login>
+                  </Route>
+                  {/* <PrivateRoute path="/booking/:serviceId">
+                     <Booking></Booking>
+                  </PrivateRoute> */}
+                  {/* <Route path="*">
+                     <NotFound></NotFound>
+                  </Route> */}
+               </Switch>
+            </Router>
+         </AuthProvider>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyANC9jv-o327q5DwCC6VhWPSQemmfGWpao",
+   authDomain: "eldery-care.firebaseapp.com",
+   projectId: "eldery-care",
+   storageBucket: "eldery-care.appspot.com",
+   messagingSenderId: "920333759690",
+   appId: "1:920333759690:web:e0cc95afcb9cceebac3033",
+};
+
+export default firebaseConfig;
